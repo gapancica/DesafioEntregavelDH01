b74e9d46b0e62f1491c3c69a095ad724b3450a81
Gabriel Pancica <gapancica@gmail.com>
2020-03-17 22:11:02 -0300

    Classe Professor criada

---
+++ src/Professor.java
@@ -0,0 +1,68 @@
+public class Professor {
+
+    private String
+            nome,
+            sobrenome;
+
+    private Integer
+            tempoDeCasa,
+            codigoDeProfessor;
+
+    public Professor() {
+
+    }
+
+    public Professor(String nome, String sobrenome, Integer tempoDeCasa, Integer codigoDeProfessor) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.tempoDeCasa = tempoDeCasa;
+        this.codigoDeProfessor = codigoDeProfessor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Professor)) return false;
+
+        Professor professor = (Professor) o;
+
+        return codigoDeProfessor.equals(professor.codigoDeProfessor);
+    }
+
+    @Override
+    public int hashCode() {
+        return codigoDeProfessor.hashCode();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Integer getTempoDeCasa() {
+        return tempoDeCasa;
+    }
+
+    public void setTempoDeCasa(Integer tempoDeCasa) {
+        this.tempoDeCasa = tempoDeCasa;
+    }
+
+    public Integer getCodigoDeProfessor() {
+        return codigoDeProfessor;
+    }
+
+    public void setCodigoDeProfessor(Integer codigoDeProfessor) {
+        this.codigoDeProfessor = codigoDeProfessor;
+    }
+}
